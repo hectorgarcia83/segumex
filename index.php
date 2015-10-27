6b912edbe4617b8269776be4dc4dc852d1d8b39c
hectorgarcia83 <hectorgarcia83@gmail.com>
2015-10-26 23:28:45 -0700

    video funcional
    El video está funcional, falta que la barra sticky funcione bien, a
    veces no funciona, las demás secciones están como imágenes.

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Segumex</title>
 
-	<link rel="stylesheet" href="css/app.css">
 	<link rel="stylesheet" href="assets/foundation/css/foundation.css">
+	<link rel="stylesheet" href="css/app.css">
+	<script src="assets/foundation/js/vendor/modernizr.js"></script>
 
 	<!-- Fonts
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'> -->	
@@ -42,46 +43,79 @@
 			<div class="vid-overlay"></div>
 			<div class="horizontalinner">
 				<div class="row">
-					<div class="small-12 small-centered columns section-logo text-center">
+					<div class="small-3 small-centered columns section-logo text-center">
 						<img src="media/images/logo.png" alt="">
 					</div>
 				</div>
 				<div class="row">
-					<div class="small-12 small-centered columns section-title text-center">
+					<div class="small-9 small-centered columns section-title text-center">
 						¿QUIÉRES <span>PROTEGER</span> TU<br><span>PATRIMONIO</span>?
 					</div>
 				</div>
 				<div class="row">
-					<div class="small-12 small-centered columns section-sigueme text-center">
+					<div class="small-3 small-centered columns section-sigueme text-center">
 						<img src="media/images/sigueme.png" alt="">
 					</div>
 				</div>
 			</div>
 			<div id="video-wrap">
-			    <video id="my-video" preload="auto"  loop="loop" muted="muted" poster="media/videos/videoejemplo.jpg">
-			        <source src="media/videos/videoejemplo.mp4" type="video/mp4">
-			        <source src="media/videos/videoejemplo.mp4" type="video/webm">
+			    <video id="my-video" preload="auto" autoplay loop muted poster="media/videos/videoejemplo.jpg">
+			        <source src="media/videos/top_video.mp4" type="video/mp4">
+			        <source src="media/videos/top_video.webm" type="video/webm">
+			        	<source src="media/videos/top_video.ogg" type="video/ogg">
 			    </video>
 			</div>	
 		</div>
 	</section>
 	
 	<section id="header-menu">
-		<div id="top-menu">
-			
-		</div>
+		
+			<div class="sticky top-menu">
+				<nav id="top-bar" data-topbar role="navigation" data-options="sticky_on: large">
+					<div class="small-3 columns">
+						<img src="media/images/logo_horizontal.png" alt="">
+					</div>
+					<div class="small-6 columns text-center" id="menu-contactanos">
+						<span>CONTÁCTANOS</span>
+					</div>
+					<div class="small-3 columns text-right" id="menu-bar">
+						<a href="#" class="button radius nav-togle" >
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+					</div>
+					
+				</nav>
+			</div>
+		
 	</section>
 	
-	
+	<section id="contenido">
+		<div class="cajas">
+			<img src="media/images/cajas.png" alt="">
+		</div>
+		<div class="conocenos">
+			<img src="media/images/conocenos.png" alt="">
+		</div>
+		<div class="galeria">
+			
+		</div>
+		<div class="footer">
+			<img src="media/images/footer.png" alt="">
+		</div>
+	</section>
 
 
 	<!-- Scripts -->
 	<script src="assets/foundation/js/vendor/jquery.js"></script>
+	<script src="assets/foundation/js/foundation.min.js"></script>
 	<script src="assets/backgroundVideo.min.js"></script>
 
 	<script>
 	    $(document).ready(function() {
 	        $('#my-video').backgroundVideo();
+	        $(document).foundation();
 	    });
 	</script>
 </body>
