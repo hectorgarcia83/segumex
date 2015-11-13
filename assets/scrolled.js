b77e5db6e189c7110dfd95193c8b5415d0589e7b
hectorgarcia83 <hectorgarcia83@gmail.com>
2015-11-12 23:50:02 -0700

    Se pone en un archivo js las funciones del scrolled menu

--- assets/scrolled.js
+++ assets/scrolled.js
@@ -1,112 +1,28 @@
-(function ($) {
+$window = $(window);
 
-   var $window = $(window),
-       _watch = [],
-       _buffer;
+$window.scroll(function() {
+  var distanciaHeader = 0;
+  var distanciaTiposCajas = $('#header').height();
+  var distanciaAltaSeguridad = $('#header').height() + $('#galeriacajas').height()+110;
+  var distanciaGaleria = distanciaAltaSeguridad + $('#altaseguridad').height()+45;
+  var distanciaFormasDePago = distanciaGaleria + $('#galeriaimagenes').height()+55;
+  var distanciaNosotros = distanciaFormasDePago + $('#formasdepago').height()+125;
+  var distanciaFooter = distanciaNosotros + $('#nosotros').height()+110;
 
-   function test($el) {
-      var docViewTop = $window.scrollTop(),
-          docViewBottom = docViewTop + $window.height(),
-          elemTop = $el.offset().top,
-          elemBottom = elemTop + $el.height();
-
-      return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
-                && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
-   }
-
-   $window.on('scroll', function ( e ) {
-
-         if ( !_buffer ) {
-
-            _buffer = setTimeout(function () {
-
-               checkInView( e );
-
-               _buffer = null;
-
-            }, 300);
-         }
-
-      });
-
-   function checkInView( e ) {
-
-      $.each(_watch, function () {
-
-         if ( test( this.element ) ) {
-            if ( !this.invp ) {
-               this.invp = true;
-               if ( this.options.scrolledin ) 
-                   this.options.scrolledin.call( this.element, e );
-                
-               this.element.trigger( 'scrolledin', e );
-            }
-         } else if ( this.invp ) {
-            this.invp = false;
-            if ( this.options.scrolledout ) 
-                this.options.scrolledout.call( this.element, e );
-             
-            this.element.trigger( 'scrolledout', e );
-         }
-      });
-   }
-
-   function monitor( element, options ) {
-      var item = { element: element, options: options, invp: false };
-      _watch.push(item);
-      return item;
-   }
-
-   function unmonitor( item ) {
-      for ( var i=0;i<_watch.length;i++ ) {
-         if ( _watch[i] === item ) {
-            _watch.splice( i, 1 );
-            item.element = null;
-            break;
-         }
-      }
-      console.log( _watch );
-   }
-
-   var pluginName = 'scrolledIntoView',
-       settings = {
-         scrolledin: null,
-         scrolledout: null
-       }
-
-
-   $.fn[pluginName] = function( options ) {
-
-         var options = $.extend({}, settings, options);
-
-         this.each( function () {
-
-               var $el = $(this),
-                   instance = $.data( this, pluginName );
-
-               if ( instance ) {
-                  instance.options = options;
-               } else {
-                  $.data( this, pluginName, monitor( $el, options ) );
-                  $el.on( 'remove', $.proxy( function () {
-
-                        $.removeData(this, pluginName);
-                        unmonitor( instance );
-
-                     }, this ) );
-               }
-            });
-
-         return this;
-      }
-
-
-})(jQuery);
-
-
-$('section.scrolling')
-      .scrolledIntoView()
-      .on('scrolledin', function () { 
-              console.log( 'in '+$(this).attr('id') ); })
-      .on('scrolledout', function () { 
-              console.log( 'out '+$(this).attr('id') ); });
+  position = $window.scrollTop();
+  if ( position >= distanciaHeader && position < distanciaTiposCajas ) {
+    $('#menu-titulo').html('');
+  }else if ( position >= distanciaTiposCajas && position < distanciaAltaSeguridad ) {
+    $('#menu-titulo').html('TIPOS DE CAJAS');
+  }else if ( position >= distanciaAltaSeguridad && position < distanciaGaleria ) {
+    $('#menu-titulo').html('ALTA SEGURIDAD');
+  }else if ( position >= distanciaGaleria && position < distanciaFormasDePago) {
+    $('#menu-titulo').html('GALERIA DE IMÁGENES');
+  }else if ( position >= distanciaFormasDePago && position < distanciaNosotros) {
+    $('#menu-titulo').html('FORMAS DE PAGO');
+  }else if ( position >= distanciaNosotros && position < distanciaFooter) {
+    $('#menu-titulo').html('NOSOTROS');
+  }else if ( position >= distanciaFooter ) {
+    $('#menu-titulo').html('CONTÁCTANOS');
+  }
+});
--- index.php
+++ index.php
@@ -59,38 +59,14 @@
 	<script src="assets/slidemenu/js/menu.js"></script>
 	<script src="assets/backgroundVideo.min.js"></script>
 	<script src="assets/jquery.sticky.js"></script>
+	<script src="assets/scrolled.js"></script>
 	<script src="js/main.js"></script>
 	<script src="js/galeria_cajas.js"></script>
+	<!--[if lt IE 9]>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+		<script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+		<script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+	<![endif]-->
 </body>
 </html>
-<script>
-
-$window = $(window);
-
-$window.scroll(function() {
-	var distanciaHeader = 0;
-	var distanciaTiposCajas = $('#header').height();
-	var distanciaAltaSeguridad = $('#header').height() + $('#galeriacajas').height()+110;
-	var distanciaGaleria = distanciaAltaSeguridad + $('#altaseguridad').height()+45;
-	var distanciaFormasDePago = distanciaGaleria + $('#galeriaimagenes').height()+55;
-	var distanciaNosotros = distanciaFormasDePago + $('#formasdepago').height()+125;
-	var distanciaFooter = distanciaNosotros + $('#nosotros').height()+110;
-
-	position = $window.scrollTop();
-	if ( position >= distanciaHeader && position < distanciaTiposCajas ) {
-		$('#menu-titulo').html('');
-	}else if ( position >= distanciaTiposCajas && position < distanciaAltaSeguridad ) {
-		$('#menu-titulo').html('TIPOS DE CAJAS');
-	}else if ( position >= distanciaAltaSeguridad && position < distanciaGaleria ) {
-		$('#menu-titulo').html('ALTA SEGURIDAD');
-	}else if ( position >= distanciaGaleria && position < distanciaFormasDePago) {
-		$('#menu-titulo').html('GALERIA DE IMÁGENES');
-	}else if ( position >= distanciaFormasDePago && position < distanciaNosotros) {
-		$('#menu-titulo').html('FORMAS DE PAGO');
-	}else if ( position >= distanciaNosotros && position < distanciaFooter) {
-		$('#menu-titulo').html('NOSOTROS');
-	}else if ( position >= distanciaFooter ) {
-		$('#menu-titulo').html('CONTÁCTANOS');
-	}
-});
-</script>
